74b1808a6b622aa2881cc29ffa6c0376e81768e8
Snow Blind <snowy73@github.com>
2013-04-20 01:11:01 +0300

    got rid of user routes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
   , app = express()
@@ -36,7 +35,6 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 server.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
