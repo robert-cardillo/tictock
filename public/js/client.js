ce6c4c8a4d290751c3ecfda18bb0290de7f48e03
Snow Blind <snowy.tmp@gmail.com>
2013-04-26 17:02:33 +0300

    applied closure patttern for client.js

--- public/js/client.js
+++ public/js/client.js
@@ -1,31 +1,46 @@
-(function (){
+var game = (function (){
     var socket;
-    connect();
+    connect_socket();
 
-    function connect(){
-        socket = io.connect('//');
-        connecting();
+    function connect_socket(){
+        socket = io.connect();  // TODO: socket server URI needed..
+        socket_connecting();
 
         //  [socket hooks]
         socket.on('connect', function(){
-            connected();
+            socket_connected();
         });
 
         socket.on('disconnect', function(){
-            disconnected()
+            socket_disconnected();
+        });
+
+        socket.on('error', function(err){
+            socket_error(err);
         });
         //  [/socket hooks]
     }
 
-    function connecting(){
+    function socket_connecting(){
         $('body').append('<p>Connecting...</p>');
     }
 
-    function connected(){
+    function socket_connected(){
         $('body').append('<p>Connected.</p>');
     }
 
-    function disconnected(){
+    function socket_disconnected(){
         $('body').append('<p>Disconnected!</p>');
     }
+
+    function socket_error(err){
+        $('body').append('<p>Error!!!</p>');
+    }
+
+    return {
+        publicVar: 73,
+        publicMethod: function(){
+            console.log(socket);
+        }
+    };
 }());
